f2c383e2fc4bb2275ff5811f4aad45500f24271e
ege-sahin <sahin.ege98@gmail.com>
2021-12-24 23:08:48 +0300

    trying to merge gouraud and phong 2

--- triangle.js
+++ triangle.js
@@ -6,7 +6,7 @@ var lines = false;
 
 var canvas;
 var gl;
- 
+var program;
 
 var normalsArray = [];
 
@@ -21,11 +21,6 @@ var left = -12.0;
 var right = 12.0;
 var ytop =12.0;
 var bottom = -12.0;
-
-var va = vec4(0.0, 0.0, -1.0,1);
-var vb = vec4(0.0, 0.942809, 0.333333, 1);
-var vc = vec4(-0.816497, -0.471405, 0.333333, 1);
-var vd = vec4(0.816497, -0.471405, 0.333333,1);
     
 var lightPosition = vec4(1.0, 1.0, 1.0, 0.0 );
 var lightAmbient = vec4(0.2, 0.2, 0.2, 1.0 );
@@ -79,60 +74,109 @@ window.onload = function init() {
     
     gl.enable(gl.DEPTH_TEST);
 
-    //
-    //  Load shaders and initialize attribute buffers
-    //
-    var program = initShaders( gl, "vertex-shader", "fragment-shader" );
-    gl.useProgram( program );
+    document.getElementById("Button6").onclick = function(){
+        program = initShaders( gl, "gouraud-vertex-shader", "gouraud-fragment-shader" );
+        gl.useProgram( program );
+        ambientProduct = mult(lightAmbient, materialAmbient);
+        diffuseProduct = mult(lightDiffuse, materialDiffuse);
+        specularProduct = mult(lightSpecular, materialSpecular);
+
+        createTorus(2,5,10,5,0.6,0.75,0.35);
+        adjustPoints(180,11);
+
+        var nBuffer = gl.createBuffer();
+        gl.bindBuffer( gl.ARRAY_BUFFER, nBuffer);
+        gl.bufferData( gl.ARRAY_BUFFER, flatten(normalsArray), gl.STATIC_DRAW );
+        
+        var vNormal = gl.getAttribLocation( program, "vNormal" );
+        gl.vertexAttribPointer( vNormal, 4, gl.FLOAT, false, 0, 0 );
+        gl.enableVertexAttribArray( vNormal);
+
+        var vBuffer = gl.createBuffer();
+        gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
+        gl.bufferData(gl.ARRAY_BUFFER, flatten(outerSurfacePoints), gl.STATIC_DRAW);
+        
+        var vPosition = gl.getAttribLocation( program, "vPosition");
+        gl.vertexAttribPointer(vPosition, 4, gl.FLOAT, false, 0, 0);
+        gl.enableVertexAttribArray(vPosition);
+        
+        modelViewMatrixLoc = gl.getUniformLocation( program, "modelViewMatrix" );
+        projectionMatrixLoc = gl.getUniformLocation( program, "projectionMatrix" );
+        normalMatrixLoc = gl.getUniformLocation( program, "normalMatrix" );
+
+        document.getElementById("Button0").onclick = function(){radius *= 2.0;};
+        document.getElementById("Button1").onclick = function(){radius *= 0.5;};
+        document.getElementById("Button2").onclick = function(){theta += dr;};
+        document.getElementById("Button3").onclick = function(){theta -= dr;};
+        document.getElementById("Button4").onclick = function(){phi += dr;};
+        document.getElementById("Button5").onclick = function(){phi -= dr;};
+
+
+        gl.uniform4fv( gl.getUniformLocation(program, 
+        "ambientProduct"),flatten(ambientProduct) );
+        gl.uniform4fv( gl.getUniformLocation(program, 
+        "diffuseProduct"),flatten(diffuseProduct) );
+        gl.uniform4fv( gl.getUniformLocation(program, 
+        "specularProduct"),flatten(specularProduct) );	
+        gl.uniform4fv( gl.getUniformLocation(program, 
+        "lightPosition"),flatten(lightPosition) );
+        gl.uniform1f( gl.getUniformLocation(program, 
+        "shininess"),materialShininess );
+        render();
     
+    }
+    document.getElementById("Button7").onclick = function(){
+        program = initShaders( gl, "phong-vertex-shader", "phong-fragment-shader" );
+        gl.useProgram( program );
+        ambientProduct = mult(lightAmbient, materialAmbient);
+        diffuseProduct = mult(lightDiffuse, materialDiffuse);
+        specularProduct = mult(lightSpecular, materialSpecular);
 
-    ambientProduct = mult(lightAmbient, materialAmbient);
-    diffuseProduct = mult(lightDiffuse, materialDiffuse);
-    specularProduct = mult(lightSpecular, materialSpecular);
+        createTorus(2,5,10,5,0.6,0.75,0.35);
+        adjustPoints(180,11);
 
-    createTorus(2,5,10,5,0.6,0.75,0.35);
-    adjustPoints(180,11);
+        var nBuffer = gl.createBuffer();
+        gl.bindBuffer( gl.ARRAY_BUFFER, nBuffer);
+        gl.bufferData( gl.ARRAY_BUFFER, flatten(normalsArray), gl.STATIC_DRAW );
+        
+        var vNormal = gl.getAttribLocation( program, "vNormal" );
+        gl.vertexAttribPointer( vNormal, 4, gl.FLOAT, false, 0, 0 );
+        gl.enableVertexAttribArray( vNormal);
 
-    var nBuffer = gl.createBuffer();
-    gl.bindBuffer( gl.ARRAY_BUFFER, nBuffer);
-    gl.bufferData( gl.ARRAY_BUFFER, flatten(normalsArray), gl.STATIC_DRAW );
+        var vBuffer = gl.createBuffer();
+        gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
+        gl.bufferData(gl.ARRAY_BUFFER, flatten(outerSurfacePoints), gl.STATIC_DRAW);
+        
+        var vPosition = gl.getAttribLocation( program, "vPosition");
+        gl.vertexAttribPointer(vPosition, 4, gl.FLOAT, false, 0, 0);
+        gl.enableVertexAttribArray(vPosition);
+        
+        modelViewMatrixLoc = gl.getUniformLocation( program, "modelViewMatrix" );
+        projectionMatrixLoc = gl.getUniformLocation( program, "projectionMatrix" );
+        normalMatrixLoc = gl.getUniformLocation( program, "normalMatrix" );
+
+        document.getElementById("Button0").onclick = function(){radius *= 2.0;};
+        document.getElementById("Button1").onclick = function(){radius *= 0.5;};
+        document.getElementById("Button2").onclick = function(){theta += dr;};
+        document.getElementById("Button3").onclick = function(){theta -= dr;};
+        document.getElementById("Button4").onclick = function(){phi += dr;};
+        document.getElementById("Button5").onclick = function(){phi -= dr;};
+
+
+        gl.uniform4fv( gl.getUniformLocation(program, 
+        "ambientProduct"),flatten(ambientProduct) );
+        gl.uniform4fv( gl.getUniformLocation(program, 
+        "diffuseProduct"),flatten(diffuseProduct) );
+        gl.uniform4fv( gl.getUniformLocation(program, 
+        "specularProduct"),flatten(specularProduct) );	
+        gl.uniform4fv( gl.getUniformLocation(program, 
+        "lightPosition"),flatten(lightPosition) );
+        gl.uniform1f( gl.getUniformLocation(program, 
+        "shininess"),materialShininess );
+        render();
+    }  
+
     
-    var vNormal = gl.getAttribLocation( program, "vNormal" );
-    gl.vertexAttribPointer( vNormal, 4, gl.FLOAT, false, 0, 0 );
-    gl.enableVertexAttribArray( vNormal);
-
-    var vBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER, flatten(outerSurfacePoints), gl.STATIC_DRAW);
-    
-    var vPosition = gl.getAttribLocation( program, "vPosition");
-    gl.vertexAttribPointer(vPosition, 4, gl.FLOAT, false, 0, 0);
-    gl.enableVertexAttribArray(vPosition);
-    
-    modelViewMatrixLoc = gl.getUniformLocation( program, "modelViewMatrix" );
-    projectionMatrixLoc = gl.getUniformLocation( program, "projectionMatrix" );
-    normalMatrixLoc = gl.getUniformLocation( program, "normalMatrix" );
-
-    document.getElementById("Button0").onclick = function(){radius *= 2.0;};
-    document.getElementById("Button1").onclick = function(){radius *= 0.5;};
-    document.getElementById("Button2").onclick = function(){theta += dr;};
-    document.getElementById("Button3").onclick = function(){theta -= dr;};
-    document.getElementById("Button4").onclick = function(){phi += dr;};
-    document.getElementById("Button5").onclick = function(){phi -= dr;};
-
-
-    gl.uniform4fv( gl.getUniformLocation(program, 
-       "ambientProduct"),flatten(ambientProduct) );
-    gl.uniform4fv( gl.getUniformLocation(program, 
-       "diffuseProduct"),flatten(diffuseProduct) );
-    gl.uniform4fv( gl.getUniformLocation(program, 
-       "specularProduct"),flatten(specularProduct) );	
-    gl.uniform4fv( gl.getUniformLocation(program, 
-       "lightPosition"),flatten(lightPosition) );
-    gl.uniform1f( gl.getUniformLocation(program, 
-       "shininess"),materialShininess );
-
-    render();
 }
 
 /*function adjustPoints(number_of_points_on_knot_curve, number_of_points_on_each_circle){
