38f94803fa491c30ced54ece697b8d3b7ee826b9
ege-sahin <sahin.ege98@gmail.com>
2021-12-28 21:30:52 +0300

    texture mapping 2nd try

--- triangle.js
+++ triangle.js
@@ -4,11 +4,8 @@ var gl;
 var points = [];
 var outerSurfacePoints = [];
 
-var texSize = 64;
-
 var program;
 
-var pointsArray = [];
 var colorsArray = [];
 var texCoordsArray = [];
 
@@ -30,6 +27,9 @@ function triangle(a, b, c) {
     */
 
     outerSurfacePoints.push(a);
+    colorsArray.push(vec4(1,0,0,1));
+    colorsArray.push(vec4(1,0,0,1));
+    colorsArray.push(vec4(1,0,0,1));
     outerSurfacePoints.push(b);      
     outerSurfacePoints.push(c);
 }
@@ -37,22 +37,35 @@ function triangle(a, b, c) {
 function adjustPoints(number_of_points_on_knot_curve, number_of_points_on_each_circle){
     for(i = 0; i < number_of_points_on_knot_curve - 1; i++){
         for(j = 0; j < number_of_points_on_each_circle - 1; j++){
-
-            triangle(points[i*number_of_points_on_each_circle + j] , points[i*number_of_points_on_each_circle + j+number_of_points_on_each_circle], points[i*number_of_points_on_each_circle + j+1]);
-            triangle(points[i*number_of_points_on_each_circle + j+number_of_points_on_each_circle], points[i*number_of_points_on_each_circle + j+number_of_points_on_each_circle + 1], points[i*number_of_points_on_each_circle + j+1] );
-             
+            triangle(points[i*number_of_points_on_each_circle + j],
+                    points[i*number_of_points_on_each_circle + j+number_of_points_on_each_circle],
+                    points[i*number_of_points_on_each_circle + j+1]);// sağ el kuralı
+            triangle(points[i*number_of_points_on_each_circle + j+number_of_points_on_each_circle],
+                    points[i*number_of_points_on_each_circle + j+number_of_points_on_each_circle + 1],
+                    points[i*number_of_points_on_each_circle + j+1] );// sağ el kuralı
         }
-        triangle(points[i*number_of_points_on_each_circle + j],points[i*number_of_points_on_each_circle + j + number_of_points_on_each_circle],points[i*number_of_points_on_each_circle + j - number_of_points_on_each_circle + 1]);
-
-        triangle(points[i*number_of_points_on_each_circle + j - number_of_points_on_each_circle + 1],points[i*number_of_points_on_each_circle + j+number_of_points_on_each_circle],points[i*number_of_points_on_each_circle + j + 1]);
+        triangle(points[i*number_of_points_on_each_circle + j],
+                points[i*number_of_points_on_each_circle + j + number_of_points_on_each_circle],
+                points[i*number_of_points_on_each_circle + j - number_of_points_on_each_circle + 1]); // sağ el kuralı
+        triangle(points[i*number_of_points_on_each_circle + j - number_of_points_on_each_circle + 1],
+                points[i*number_of_points_on_each_circle + j+number_of_points_on_each_circle],
+                points[i*number_of_points_on_each_circle + j + 1]);// sağ el kuralı
 
     }    
     for(i = 0; i < number_of_points_on_each_circle - 1; i++){
-        triangle(points[i], points[(number_of_points_on_knot_curve - 1) * number_of_points_on_each_circle + i + 1], points[(number_of_points_on_knot_curve - 1) * number_of_points_on_each_circle + i]);
-        triangle(points[(number_of_points_on_knot_curve - 1) * number_of_points_on_each_circle + i + 1], points[i], points[i + 1]);
+        triangle(points[i],
+                points[(number_of_points_on_knot_curve - 1) * number_of_points_on_each_circle + i + 1],
+                points[(number_of_points_on_knot_curve - 1) * number_of_points_on_each_circle + i]);// sağ el kuralı
+        triangle(points[(number_of_points_on_knot_curve - 1) * number_of_points_on_each_circle + i + 1],
+                points[i],
+                points[i + 1]);// sağ el kuralı
     }
-    triangle(points[number_of_points_on_each_circle - 1],points[(number_of_points_on_knot_curve - 1) * number_of_points_on_each_circle + number_of_points_on_each_circle - 1],points[(number_of_points_on_knot_curve - 1) * number_of_points_on_each_circle]);
-    triangle(points[(number_of_points_on_knot_curve - 1) * number_of_points_on_each_circle]  , points[number_of_points_on_each_circle - 1], points[0]);
+    triangle(points[number_of_points_on_each_circle - 1],
+            points[(number_of_points_on_knot_curve - 1) * number_of_points_on_each_circle],
+            points[(number_of_points_on_knot_curve - 1) * number_of_points_on_each_circle + number_of_points_on_each_circle - 1]);
+    triangle(points[(number_of_points_on_knot_curve - 1) * number_of_points_on_each_circle],
+            points[number_of_points_on_each_circle - 1],
+            points[0]);// sağ el kuralı
 
 }
 
@@ -85,33 +98,64 @@ function createTorus(p, q1, q2, m1,r1,r2,s1)
 }
 
 function adjustTextureCoor(number_of_points_on_knot_curve, number_of_points_on_each_circle){
-    for( i = 0; i <= 1; i += 1/number_of_points_on_knot_curve){
-        for(j = 0; j <= 1; j += 1/number_of_points_on_each_circle){
+    var innerLoop = 0;
+    for( i = 0; i < number_of_points_on_knot_curve; i++){
+        for(j = 0; j < number_of_points_on_each_circle; j++){
             texCoordsArray.push(vec2(i,j));
         }
+        innerLoop++;
     }
+    console.log(innerLoop);
 }
+
+var texSize = 64;
+
+// Create a checkerboard pattern using floats
+
+    
+var image1 = new Array()
+    for (var i =0; i<texSize; i++)  image1[i] = new Array();
+    for (var i =0; i<texSize; i++) 
+        for ( var j = 0; j < texSize; j++) 
+           image1[i][j] = new Float32Array(4);
+    for (var i =0; i<texSize; i++) for (var j=0; j<texSize; j++) {
+        var c = (((i & 0x8) == 0) ^ ((j & 0x8)  == 0));
+        image1[i][j] = [c, c, c, 1];
+    }
+
+// Convert floats to ubytes for texture
+
+var image2 = new Uint8Array(4*texSize*texSize);
+
+    for ( var i = 0; i < texSize; i++ ) 
+        for ( var j = 0; j < texSize; j++ ) 
+           for(var k =0; k<4; k++) 
+                image2[4*texSize*i+4*j+k] = 255*image1[i][j][k];
+        
+
+
 var xAxis = 0;
 var yAxis = 1;
 var zAxis = 2;
 var axis = xAxis;
-var theta = [45.0, 45.0, 45.0];
+
+var theta = [0,0,0];
 
 var thetaLoc;
 
-function configureTexture( image ) {
+function configureTexture(image) {
     texture = gl.createTexture();
+    gl.activeTexture( gl.TEXTURE0 );
     gl.bindTexture( gl.TEXTURE_2D, texture );
     gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
-    gl.texImage2D( gl.TEXTURE_2D, 0, gl.RGB, 
-         gl.RGB, gl.UNSIGNED_BYTE, image );
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, texSize, texSize, 0, 
+        gl.RGBA, gl.UNSIGNED_BYTE, image);
     gl.generateMipmap( gl.TEXTURE_2D );
     gl.texParameteri( gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, 
-                      gl.NEAREST_MIPMAP_LINEAR );
+        gl.NEAREST_MIPMAP_LINEAR );
     gl.texParameteri( gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST );
-    
-    gl.uniform1i(gl.getUniformLocation(program, "texture"), 0);
 }
+
 
 window.onload = function init() {
 
@@ -128,62 +172,55 @@ window.onload = function init() {
     //
     //  Load shaders and initialize attribute buffers
     //
-    program = initShaders( gl, "vertex-shader", "fragment-shader" );
+    var program = initShaders( gl, "vertex-shader", "fragment-shader" );
     gl.useProgram( program );
     
     createTorus(2,5,10,5,0.6, 0.75, 0.35)
     adjustPoints(180,11);
-    adjustTextureCoor(180,11);
+    console.log(outerSurfacePoints.length);
+    adjustTextureCoor(1080,11);
+    console.log(texCoordsArray.length);
+
+    var cBuffer = gl.createBuffer();
+    gl.bindBuffer( gl.ARRAY_BUFFER, cBuffer);
+    gl.bufferData( gl.ARRAY_BUFFER, flatten(colorsArray), gl.STATIC_DRAW );
+    var vColor = gl.getAttribLocation( program, "vColor" );
+    gl.vertexAttribPointer(vColor, 4, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(vColor);
 
     var vBuffer = gl.createBuffer();
-    gl.bindBuffer( gl.ARRAY_BUFFER, vBuffer );
-    gl.bufferData( gl.ARRAY_BUFFER, flatten(outerSurfacePoints), gl.STATIC_DRAW );
-    
+    gl.bindBuffer( gl.ARRAY_BUFFER, vBuffer);
+    gl.bufferData( gl.ARRAY_BUFFER, flatten(outerSurfacePoints), gl.STATIC_DRAW);
     var vPosition = gl.getAttribLocation( program, "vPosition" );
-    gl.vertexAttribPointer( vPosition, 4, gl.FLOAT, false, 0, 0 );
-    gl.enableVertexAttribArray( vPosition );
+    gl.vertexAttribPointer( vPosition, 4, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(vPosition);
     
     var tBuffer = gl.createBuffer();
-    gl.bindBuffer( gl.ARRAY_BUFFER, tBuffer );
+    gl.bindBuffer( gl.ARRAY_BUFFER, tBuffer);
     gl.bufferData( gl.ARRAY_BUFFER, flatten(texCoordsArray), gl.STATIC_DRAW );
-    
-    var vTexCoord = gl.getAttribLocation( program, "vTexCoord" );
-    gl.vertexAttribPointer( vTexCoord, 2, gl.FLOAT, false, 0, 0 );
-    gl.enableVertexAttribArray( vTexCoord );
+    var vTexCoord = gl.getAttribLocation( program, "vTexCoord");
+    gl.vertexAttribPointer(vTexCoord, 2, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(vTexCoord);
 
-    //
-    // Initialize a texture
-    //
-
-    //var image = new Image();
-    //image.onload = function() { 
-     //   configureTexture( image );
-    //}
-    //image.src = "SA2011_black.gif"
-
-
-    var image = document.getElementById("texImage");
- 
-    configureTexture( image );
+    configureTexture(image2);
 
     thetaLoc = gl.getUniformLocation(program, "theta"); 
     
     document.getElementById("ButtonX").onclick = function(){axis = xAxis;};
     document.getElementById("ButtonY").onclick = function(){axis = yAxis;};
     document.getElementById("ButtonZ").onclick = function(){axis = zAxis;};
-       
-    render();
 
     projectionMatrix = ortho(-15,15,-15,15,-15,15);
     var projectionMatrixLoc = gl.getUniformLocation( program, "projectionMatrix" );
     gl.uniformMatrix4fv(projectionMatrixLoc, false, flatten(projectionMatrix) );
- 
+                              
+    render();
 }
 
-var render = function(){
+var render = function() {
     gl.clear( gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-    theta[axis] += 2.0;
-    gl.uniform3fv(thetaLoc, flatten(theta));
+    //theta[axis] += 2.0;
+    gl.uniform3fv(thetaLoc, theta);
     gl.drawArrays( gl.TRIANGLES, 0, outerSurfacePoints.length );
     requestAnimFrame(render);
 }
